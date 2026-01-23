309aaea67a6c1a37ee71c1fc1dc41b1f3b91a2f0
ynoma-cell <y.noma@rasa-jp.co.jp>
2026-01-23 10:45:36 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+        build: {
+        outDir: 'docs', // 出力先を dist から docs に変更
+        emptyOutDir: true, // ビルド時に docs フォルダを一度空にする（推奨）
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
